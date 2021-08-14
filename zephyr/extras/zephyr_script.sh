0e258760154c4a8fbef369eb0fe3192a846d50ea
gabopushups <65993380+gabopushups@users.noreply.github.com>
2021-08-13 20:38:14 -0500

    Update zephyr_script.sh

--- zephyr/extras/zephyr_script.sh
+++ zephyr/extras/zephyr_script.sh
@@ -3,4 +3,4 @@
 cd
 cd zephyrproject/build/zephyr
 
-west build -b 96b_aerocore2 -p='always' ~/Documents/UniversidadNacional/ProyectoDeGrado/Firmware_keepBreathing/zephyr/08_controlador_final
+west build -b 96b_aerocore2 -p='always' ~/ruta/a/carpeta/de/proyecto
